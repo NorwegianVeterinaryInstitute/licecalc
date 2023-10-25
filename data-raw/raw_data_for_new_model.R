8052dec2a6f267e266c0f496d93f28e1998cde78
novica <nnovica@gmail.com>
2023-10-25 12:20:08 +0200

    update package data

--- data-raw/raw_data_for_new_model.R
+++ data-raw/raw_data_for_new_model.R
@@ -1,8 +1,8 @@
-## code to prepare internall datasets for new lice model
-## files taken from project local storage
-## /34055_NewTechAqua/WP2/licecalc/data/
-## these data need to be documented
-## see 'https://r-pkgs.org/data.html'
+# code to prepare internall datasets for new lice model
+# files taken from project local storage
+# /34055_NewTechAqua/WP2/licecalc/data/
+# these data need to be documented
+# see 'https://r-pkgs.org/data.html'
 
 infestation_pressure_all = read.table("data-raw/LarverTotaltFra2012.txt",
                                       sep = "\t",
@@ -17,3 +17,26 @@ sea_temperature_all = read.table("data-raw/Sjoetemp.txt",
                                  dec = ",")
 
 usethis::use_data(sea_temperature_all, overwrite = TRUE)
+
+
+# data for lice age.
+# af - adult lice
+# om - mobile lice
+# fx - sessile lice
+
+adult_lice = read.table("data-raw/VoksneHunnlus.txt", sep = "\t", dec = ",")
+
+usethis::use_data(adult_lice, overwrite = TRUE)
+
+
+mobile_lice = read.table("data-raw/BevegligeLus.txt", sep = "\t", dec = ",")
+
+usethis::use_data(mobile_lice, overwrite = TRUE)
+
+# Note: there is an error in the "Fastsittendelus.txt"
+# example file, so this does not work 100 % yet
+sessile_lice = read.table("data-raw/Fastsittendelus.txt",
+                          sep = "\t",
+                          dec = ",")
+
+usethis::use_data(sessile_lice, overwrite = TRUE)
