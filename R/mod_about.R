9a38f77d13444d2c7db44e09947bf68f332af518
novica <nnovica@gmail.com>
2023-10-30 13:49:14 +0100

    feat: add home and about pages logic.

--- R/mod_about.R
+++ R/mod_about.R
@@ -1,4 +1,4 @@
-#' home UI Function
+#' about UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -7,17 +7,17 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-mod_home_ui <- function(id){
+mod_about_ui <- function(id){
   ns <- NS(id)
 
-  shiny::uiOutput(ns('home'))
+  shiny::uiOutput(ns('about'))
 
 }
 
-#' home Server Functions
+#' about Server Functions
 #'
 #' @noRd
-mod_home_server <- function(id, selected_language){
+mod_about_server <- function(id, selected_language){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
@@ -26,29 +26,28 @@ mod_home_server <- function(id, selected_language){
       get_reactive_translator(translator, selected_language())
     })
 
-    output$home <- shiny::renderUI({
+    output$about <- shiny::renderUI({
 
       if(i18n()$get_translation_language() == 'en') {
-      shiny::tagList(
-        tagList(
-          shiny::includeMarkdown(app_sys("app/www/home.md"))
-        )
-      )} else {
         shiny::tagList(
           tagList(
-            shiny::includeMarkdown(app_sys("app/www/home-nb.md"))
+            shiny::includeMarkdown(app_sys("app/www/about.md"))
           )
-        )
+        )} else {
+          shiny::tagList(
+            tagList(
+              shiny::includeMarkdown(app_sys("app/www/about-nb.md"))
+            )
+          )
 
-      }
+        }
     })
-
 
   })
 }
 
 ## To be copied in the UI
-# mod_home_ui("home_1")
+# mod_about_ui("about_1")
 
 ## To be copied in the server
-# mod_home_server("home_1")
+# mod_about_server("about_1")
